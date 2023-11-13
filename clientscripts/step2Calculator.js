32eeac9ad20d798fb269e830b09f589a86079080
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-11-13 18:42:16 -0500

    Updates

--- clientscripts/step2Calculator.js
+++ clientscripts/step2Calculator.js
@@ -9,17 +9,33 @@ const generalRate = selectedResultCard.getAttribute('data-general') ? parseFloat
 const specialJSON = selectedResultCard.getAttribute('data-special-json');
 const otherRate = selectedResultCard.getAttribute('data-other') ? parseFloat(selectedResultCard.getAttribute('data-other')) : '';
 
-// Adding Event Listeners to Calculate Duty Button
-function addCalculateDutyEventListener(callback) {
-    document.querySelector("#calculateDuty").addEventListener("click", callback);
-}
+console.log('Product Value:',productValue,'Quantity:',quantity,'ISO Code:',isoCode,'HTS No:',htsno,'General Rate:',generalRate,'Special JSON:',specialJSON,'Other Rate:',otherRate);
 
 // Main Function
-function calculateDuty(productValue, specialRates, generalRate, quantity, isoCode) {
-    const specialDutyRate = specialRates.special_json[isoCode] || "";
+function calculateDuty(productValue, specialJSON, generalRate, quantity, isoCode) {
+    const specialDutyRate = specialJSON.special_json[isoCode] || "";
     console.log('Special Duty Rate:',specialDutyRate);
     const duty = productValue * generalRate * quantity;
+    console.log('Duty:',duty);
     return duty;
 }
 
-addCalculateDutyEventListener(calculateDuty);
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the button element
+    const button = document.querySelector("#calculateDuty");
+
+    // Attach the event listener to the button
+    button.addEventListener("click", function() {
+        // Get the input values
+        const selectedResultCard = document.querySelector("#selectedResult");
+        const productValue = parseFloat(document.querySelector("#productValue").value);
+        const specialJSON = selectedResultCard.getAttribute('data-special-json');
+        const generalRate = selectedResultCard.getAttribute('data-general') ? parseFloat(selectedResultCard.getAttribute('data-general')) / 100 : '';
+        const quantity = parseFloat(document.querySelector("#quantity").value);
+        const isoCode = document.querySelector("#importingFrom").value;
+
+        // Call the calculateDuty function with the input values
+        calculateDuty(productValue, specialJSON, generalRate, quantity, isoCode);
+    });
+});
