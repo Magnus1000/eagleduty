47b0c8839bf7df6859511eb0753b2c1c3a6939ba
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-11-13 16:10:46 -0500

    Update

---
+++ clientscripts/specialTradeCountryList.js
@@ -0,0 +1,13 @@
+// A* Beneficiary Developing Countries (BDCs)
+function aStar() {
+    return ["AF", "AL", "AM", "AO", "AR", "AZ", "BA", "BF", "BI", "BJ", "BO", "BR", "BT", "BW", "BZ", "CD", "CF", "CG", "CI", "CM", "CV", "DJ", "DM", "DZ", "EC", "EG", "ER", "ET", "FJ", "GA", "GD", "GE", "GH", "GM", "GN", "GW", "GY", "HT", "ID", "IQ", "JM", "JO", "KE", "KG", "KH", "KI", "KM", "KV", "KZ", "LB", "LC", "LK", "LR", "LS", "MD", "ME", "MG", "MK", "ML", "MN", "MR", "MU", "MV", "MW", "MZ", "NA", "NE", "NG", "NP", "PG", "PH", "PK", "PY", "RS", "RW", "SB", "SL", "SN", "SO", "SR", "SS", "ST", "TD", "TG", "TH", "TL", "TN", "TO", "TV", "TZ", "UA", "UG", "UZ", "VC", "VU", "WS", "YE", "ZA", "ZM", "ZW"];
+}
+// A+ Least-developed beneficiary countries
+function aPlus () {
+    return ["AF", "AO", "BF", "BI", "BJ", "BT", "BU", "CD", "CF", "DJ", "ET", "GM", "GN", "GW", "HT", "KH", "KI", "KM", "LR", "LS", "MG", "ML", "MR", "MW", "MZ", "NE", "NP", "RW", "SB", "SL", "SN", "SO", "SS", "ST", "TD", "TG", "TL", "TV", "TZ", "UG", "VU", "WS", "YE", "ZM"];
+}
+
+// United States-Mexico-Canada Agreement (USMCA)
+function usmca() {
+    return ["CA", "MX"];
+}
