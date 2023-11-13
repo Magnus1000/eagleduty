ff4ee55d293f7e144e3795721bf5f9597fb613de
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-11-13 18:45:23 -0500

    Last update

--- clientscripts/step2Calculator.js
+++ clientscripts/step2Calculator.js
@@ -1,16 +1,3 @@
-// Reading Values of Form
-const selectedResultCard = document.querySelector("#selectedResult");
-const productValue = parseFloat(document.querySelector("#productValue").value);
-const quantity = parseFloat(document.querySelector("#quantity").value);
-const isoCode = document.querySelector("#importingFrom").value;
-const dutyTotal = document.querySelector("#dutyTotal");
-const htsno = selectedResultCard.getAttribute('data-htsno');
-const generalRate = selectedResultCard.getAttribute('data-general') ? parseFloat(selectedResultCard.getAttribute('data-general')) / 100 : '';
-const specialJSON = selectedResultCard.getAttribute('data-special-json');
-const otherRate = selectedResultCard.getAttribute('data-other') ? parseFloat(selectedResultCard.getAttribute('data-other')) : '';
-
-console.log('Product Value:',productValue,'Quantity:',quantity,'ISO Code:',isoCode,'HTS No:',htsno,'General Rate:',generalRate,'Special JSON:',specialJSON,'Other Rate:',otherRate);
-
 // Main Function
 function calculateDuty(productValue, specialJSON, generalRate, quantity, isoCode) {
     const specialDutyRate = specialJSON.special_json[isoCode] || "";
@@ -34,7 +21,8 @@ document.addEventListener("DOMContentLoaded", function() {
         const generalRate = selectedResultCard.getAttribute('data-general') ? parseFloat(selectedResultCard.getAttribute('data-general')) / 100 : '';
         const quantity = parseFloat(document.querySelector("#quantity").value);
         const isoCode = document.querySelector("#importingFrom").value;
-
+        console.log('Product Value:',productValue,'Quantity:',quantity,'ISO Code:',isoCode,'General Rate:',generalRate,'Special JSON:',specialJSON);
+        
         // Call the calculateDuty function with the input values
         calculateDuty(productValue, specialJSON, generalRate, quantity, isoCode);
     });
