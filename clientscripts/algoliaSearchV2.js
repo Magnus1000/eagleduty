81adc8b572204a638f70df63de7493303e09608a
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-11-16 10:28:05 -0500

    Updates

--- clientscripts/algoliaSearchV2.js
+++ clientscripts/algoliaSearchV2.js
@@ -58,30 +58,9 @@ function renderResults(hits) {
         const detailsElement = document.createElement('div');
         detailsElement.classList.add('details');
 
-        // Create and append the chapter element
-        const chapterElement = document.createElement('div');
-        chapterElement.classList.add('chapter');
-        const chapterImageElement = document.createElement('div');
-        chapterImageElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M334.5 446c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 88-192 0c-17.7 0-32-14.3-32-32L96 64c0-17.7-14.3-32-32-32L32 32C14.3 32 0 46.3 0 64L0 208c0 70.7 57.3 128 128 128l192 0 0 88c0 9.6 5.7 18.2 14.5 22z"/></svg>'; // Replace with your SVG code
-        chapterImageElement.classList.add('chapter-image');
-        chapterElement.appendChild(chapterImageElement);
-        const chapterNameElement = document.createElement('span');
-        chapterNameElement.textContent = hit.chapterName;
-        chapterNameElement.classList.add('chapter-name');
-        chapterElement.appendChild(chapterNameElement);
-        const chapterDescriptionElement = document.createElement('span');
-        chapterDescriptionElement.textContent = hit.chapterDescription;
-        chapterDescriptionElement.classList.add('chapter-description');
-        chapterElement.appendChild(chapterDescriptionElement);
-        detailsElement.appendChild(chapterElement);
-
         // Create and append the section element
         const sectionElement = document.createElement('div');
         sectionElement.classList.add('section');
-        const sectionImageElement = document.createElement('div');
-        sectionImageElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M334.5 446c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 88-192 0c-17.7 0-32-14.3-32-32L96 64c0-17.7-14.3-32-32-32L32 32C14.3 32 0 46.3 0 64L0 208c0 70.7 57.3 128 128 128l192 0 0 88c0 9.6 5.7 18.2 14.5 22z"/></svg>'; // Replace with your SVG code
-        sectionImageElement.classList.add('section-image');
-        sectionElement.appendChild(sectionImageElement);
         const sectionNameElement = document.createElement('span');
         sectionNameElement.textContent = hit.sectionName;
         sectionNameElement.classList.add('section-name');
@@ -91,6 +70,19 @@ function renderResults(hits) {
         sectionDescriptionElement.classList.add('section-description');
         sectionElement.appendChild(sectionDescriptionElement);
         detailsElement.appendChild(sectionElement);
+
+        // Create and append the chapter element
+        const chapterElement = document.createElement('div');
+        chapterElement.classList.add('chapter');
+        const chapterNameElement = document.createElement('span');
+        chapterNameElement.textContent = hit.chapterName;
+        chapterNameElement.classList.add('chapter-name');
+        chapterElement.appendChild(chapterNameElement);
+        const chapterDescriptionElement = document.createElement('span');
+        chapterDescriptionElement.textContent = hit.chapterDescription;
+        chapterDescriptionElement.classList.add('chapter-description');
+        chapterElement.appendChild(chapterDescriptionElement);
+        detailsElement.appendChild(chapterElement);
 
         resultItem.appendChild(detailsElement);
 
