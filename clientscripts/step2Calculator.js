c895ca412932a49f3ebe8515e7ba7049669fc2e7
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-11-17 10:02:01 -0500

    Updates

--- clientscripts/step2Calculator.js
+++ clientscripts/step2Calculator.js
@@ -93,11 +93,23 @@ document.addEventListener("DOMContentLoaded", function() {
             const selectedResultCard = document.querySelector("#selectedResult");
             const value = parseFloat(document.querySelector("#valueField").value);
 
-            // Check if data-special-json attribute exists and is not undefined
-            const specialJSON = selectedResultCard.hasAttribute('data-special-json') && selectedResultCard.getAttribute('data-special-json') !== null ? JSON.parse(selectedResultCard.getAttribute('data-special-json')) : null;
-            
-            // If the general rate is Free, set it to 0, otherwise parse the value to a number and divide by 100 to get $/unit (for amount) or a decimal (for value)
-            const generalRate = selectedResultCard.getAttribute('data-general') === 'Free' ? 0 : (selectedResultCard.getAttribute('data-general') ? parseFloat(selectedResultCard.getAttribute('data-general')) / 100 : '');
+            // Retrieve the 'data-special-json' attribute
+            const specialJSONAttr = selectedResultCard.getAttribute('data-special-json');
+            // Check if it is a valid JSON string
+            let specialJSON = null;
+            if (specialJSONAttr && specialJSONAttr !== "undefined") {
+                try {
+                    specialJSON = JSON.parse(specialJSONAttr);
+                } catch (e) {
+                    console.error("Error parsing JSON from data-special-json:", e);
+                }
+            }
+
+            // Handle the general rate attribute
+            const generalRateString = selectedResultCard.getAttribute('data-general');
+            const generalRate = generalRateString === 'Free' ? 0 : (generalRateString ? parseFloat(generalRateString) / 100 : '');
+
+            // Other input values
             const quantity = parseFloat(document.querySelector("#quantityField").value);
             const isoCode = document.querySelector("#importingFrom").value;
             const amount = parseFloat(document.querySelector("#amountField").value);
@@ -111,6 +123,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
 
 const aStar = {
     'AF': true, 'AL': true, 'AM': true, 'AO': true, 'AR': true, 'AZ': true, 'BA': true, 'BF': true, 'BI': true, 'BJ': true, 'BO': true, 'BR': true, 'BT': true, 'BW': true, 'BZ': true, 'CD': true, 'CF': true, 'CG': true, 'CI': true, 'CM': true, 'CV': true, 'DJ': true, 'DM': true, 'DZ': true, 'EC': true, 'EG': true, 'ER': true, 'ET': true, 'FJ': true, 'GA': true, 'GD': true, 'GE': true, 'GH': true, 'GM': true, 'GN': true, 'GW': true, 'GY': true, 'HT': true, 'ID': true, 'IQ': true, 'JM': true, 'JO': true, 'KE': true, 'KG': true, 'KH': true, 'KI': true, 'KM': true, 'KV': true, 'KZ': true, 'LB': true, 'LC': true, 'LK': true, 'LR': true, 'LS': true, 'MD': true, 'ME': true, 'MG': true, 'MK': true, 'ML': true, 'MN': true, 'MR': true, 'MU': true, 'MV': true, 'MW': true, 'MZ': true, 'NA': true, 'NE': true, 'NG': true, 'NP': true, 'PG': true, 'PH': true, 'PK': true, 'PY': true, 'RS': true, 'RW': true, 'SB': true, 'SL': true, 'SN': true, 'SO': true, 'SR': true, 'SS': true, 'ST': true, 'TD': true, 'TG': true, 'TH': true, 'TL': true, 'TN': true, 'TO': true, 'TV': true, 'TZ': true, 'UA': true, 'UG': true, 'UZ': true, 'VC': true, 'VU': true, 'WS': true, 'YE': true, 'ZA': true, 'ZM': true, 'ZW': true
