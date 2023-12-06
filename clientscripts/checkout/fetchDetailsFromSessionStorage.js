3d94d98329fa770f76e6bede597c94f88caf3779
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-12-06 13:13:05 -0500

    Updates

--- clientscripts/checkout/fetchDetailsFromSessionStorage.js
+++ clientscripts/checkout/fetchDetailsFromSessionStorage.js
@@ -1,21 +1,24 @@
-// Fetches the values from session storage and sets them in the DOM 
 function setSessionStorageValues() {
     const query = sessionStorage.getItem("query");
     const htsno = sessionStorage.getItem("htsno");
     const description = sessionStorage.getItem("description");
+    const uuid = localStorage.getItem('uuid');
 
     const hsQueryElement = document.getElementById("hsQuery");
     const hsCodeElement = document.getElementById("hsCode");
     const hsDescriptionElement = document.getElementById("hsDescription");
     const searchResultsCheckoutElement = document.getElementById("searchResultsCheckout");
+    const checkoutCheckboxElement = document.getElementById("checkoutCheckbox");
 
     if (query && htsno && description) {
         hsQueryElement.textContent = query;
         hsCodeElement.textContent = htsno;
         hsDescriptionElement.textContent = description;
         searchResultsCheckoutElement.style.display = "flex";
+        checkoutCheckboxElement.name = JSON.stringify([query, htsno, description, uuid]);
     } else {
         searchResultsCheckoutElement.style.display = "none";
+        checkoutCheckboxElement.name = "";
     }
 }
 
