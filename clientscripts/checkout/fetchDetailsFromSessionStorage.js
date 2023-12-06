7a12be00ed1c95df865b6e5f202776e33558586f
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-12-06 11:14:02 -0500

    Updates

--- clientscripts/checkout/fetchDetailsFromSessionStorage.js
+++ clientscripts/checkout/fetchDetailsFromSessionStorage.js
@@ -3,12 +3,20 @@ function setSessionStorageValues() {
     const query = sessionStorage.getItem("query");
     const htsno = sessionStorage.getItem("htsno");
     const description = sessionStorage.getItem("description");
-  
+
     const hsQueryElement = document.getElementById("hsQuery");
     const hsCodeElement = document.getElementById("hsCode");
     const hsDescriptionElement = document.getElementById("hsDescription");
-  
-    hsQueryElement.textContent = query;
-    hsCodeElement.textContent = htsno;
-    hsDescriptionElement.textContent = description;
+    const searchResultsCheckoutElement = document.getElementById("searchResultsCheckout");
+
+    if (query && htsno && description) {
+        hsQueryElement.textContent = query;
+        hsCodeElement.textContent = htsno;
+        hsDescriptionElement.textContent = description;
+        searchResultsCheckoutElement.style.display = "flex";
+    } else {
+        searchResultsCheckoutElement.style.display = "none";
+    }
 }
+
+window.addEventListener("DOMContentLoaded", setSessionStorageValues);
