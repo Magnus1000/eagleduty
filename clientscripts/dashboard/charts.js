352e8d1413438b3b68b7fcd003e342289055d6b1
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-12-10 10:28:22 -0500

    Updates

--- clientscripts/dashboard/charts.js
+++ clientscripts/dashboard/charts.js
@@ -5,8 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
         datasets: [{
             label: 'Daily Sales Total',
             data: [],
-            fill: false,
+            fill: true,
             borderColor: '#3772FF',
+            backgroundColor: '#ebf1ff',
             tension: 0.1
         }]
     };
@@ -16,8 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
         datasets: [{
             label: 'Daily Users Total',
             data: [],
-            fill: false,
-            borderColor: '#FF3772',
+            fill: true,
+            borderColor: '#3772FF',
+            backgroundColor: '#ebf1ff',
             tension: 0.1
         }]
     };
@@ -27,8 +29,9 @@ document.addEventListener('DOMContentLoaded', function() {
         datasets: [{
             label: 'Clicks',
             data: [],
-            fill: false,
-            borderColor: '#FFA500',
+            fill: true,
+            borderColor: '#3772FF',
+            backgroundColor: '#ebf1ff',
             tension: 0.1
         }]
     };
@@ -38,11 +41,37 @@ document.addEventListener('DOMContentLoaded', function() {
         datasets: [{
             label: 'Cost',
             data: [],
-            fill: false,
-            borderColor: '#00FF00',
+            fill: true,
+            borderColor: '#3772FF',
+            backgroundColor: '#ebf1ff',
             tension: 0.1
         }]
     };
+
+    const chartData5 = {
+        labels: [],
+        datasets: [{
+            label: 'Cost',
+            data: [],
+            fill: true,
+            borderColor: '#3772FF',
+            backgroundColor: '#ebf1ff',
+            tension: 0.1
+        }]
+    };
+
+    const chartData6 = {
+        labels: [],
+        datasets: [{
+            label: 'Cost',
+            data: [],
+            fill: true,
+            borderColor: '#3772FF',
+            backgroundColor: '#ebf1ff',
+            tension: 0.1
+        }]
+    };
+
 
     // Configuration for all charts
     const config1 = {
@@ -153,11 +182,67 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     };
 
+    const config5 = {
+        type: 'line',
+        data: chartData5,
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'day',
+                        tooltipFormat: 'MMMM D'
+                    },
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Conversion Rate'
+                    }
+                }
+            }
+        }
+    };
+
+    const config6 = {
+        type: 'line',
+        data: chartData6,
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'day',
+                        tooltipFormat: 'MMMM D'
+                    },
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'ROAS'
+                    }
+                }
+            }
+        }
+    };
+
     // Select the canvases for the charts
     const canvas1 = document.querySelector('canvas[data-custom-chart="1"]');
     const canvas2 = document.querySelector('canvas[data-custom-chart="2"]');
     const canvas3 = document.querySelector('canvas[data-custom-chart="3"]');
     const canvas4 = document.querySelector('canvas[data-custom-chart="4"]');
+    const canvas5 = document.querySelector('canvas[data-custom-chart="5"]');
+    const canvas6 = document.querySelector('canvas[data-custom-chart="6"]');
 
     // Function to create or update a chart
     function createOrUpdateChart(canvas, config, windowChartProperty) {
@@ -194,30 +279,48 @@ document.addEventListener('DOMContentLoaded', function() {
         // Update data for the first chart
         chartData1.labels = labels;
         chartData1.datasets[0].data = salesData;
+        console.log('Chart 1 Data:', chartData1);
 
         // Update data for the second chart
         chartData2.labels = labels;
         chartData2.datasets[0].data = usersData;
+        console.log('Chart 2 Data:', chartData2);
 
         // Update data for the third chart
         chartData3.labels = labels;
         chartData3.datasets[0].data = clicksData;
+        console.log('Chart 3 Data:', chartData3);
 
         // Update data for the fourth chart
         chartData4.labels = labels;
         chartData4.datasets[0].data = costData;
+        console.log('Chart 4 Data:', chartData4);
+
+        // Update data for the fifth chart
+        chartData5.labels = labels;
+        chartData5.datasets[0].data = costData;
+        console.log('Chart 5 Data:', chartData5);
+
+        // Update data for the sixth chart
+        chartData6.labels = labels;
+        chartData6.datasets[0].data = costData;
+        console.log('Chart 6 Data:', chartData6);
 
         // Create or update all charts
         createOrUpdateChart(canvas1, config1, 'myChart1');
         createOrUpdateChart(canvas2, config2, 'myChart2');
         createOrUpdateChart(canvas3, config3, 'myChart3');
         createOrUpdateChart(canvas4, config4, 'myChart4');
+        createOrUpdateChart(canvas5, config5, 'myChart5');
+        createOrUpdateChart(canvas6, config6, 'myChart6');
 
         // Update all charts
         if (window.myChart1) window.myChart1.update();
         if (window.myChart2) window.myChart2.update();
         if (window.myChart3) window.myChart3.update();
         if (window.myChart4) window.myChart4.update();
+        if (window.myChart5) window.myChart5.update();
+        if (window.myChart6) window.myChart6.update();
     })
     .catch(error => {
         console.error('Error fetching data from Airtable:', error);
