607ad869dae640127e8cd2b965dd5b7df22793da
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-12-10 10:12:14 -0500

    Updates

---
+++ clientscripts/dashboard/charts.js
@@ -0,0 +1,225 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Prepare the data for all charts
+    const chartData1 = {
+        labels: [],
+        datasets: [{
+            label: 'Daily Sales Total',
+            data: [],
+            fill: false,
+            borderColor: '#3772FF',
+            tension: 0.1
+        }]
+    };
+
+    const chartData2 = {
+        labels: [],
+        datasets: [{
+            label: 'Daily Users Total',
+            data: [],
+            fill: false,
+            borderColor: '#FF3772',
+            tension: 0.1
+        }]
+    };
+
+    const chartData3 = {
+        labels: [],
+        datasets: [{
+            label: 'Clicks',
+            data: [],
+            fill: false,
+            borderColor: '#FFA500',
+            tension: 0.1
+        }]
+    };
+
+    const chartData4 = {
+        labels: [],
+        datasets: [{
+            label: 'Cost',
+            data: [],
+            fill: false,
+            borderColor: '#00FF00',
+            tension: 0.1
+        }]
+    };
+
+    // Configuration for all charts
+    const config1 = {
+        type: 'line',
+        data: chartData1,
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'day',
+                        tooltipFormat: 'MMMM D'
+                    },
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Sales Total'
+                    }
+                }
+            }
+        }
+    };
+
+    const config2 = {
+        type: 'line',
+        data: chartData2,
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'day',
+                        tooltipFormat: 'MMMM D'
+                    },
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Users Total'
+                    }
+                }
+            }
+        }
+    };
+
+    const config3 = {
+        type: 'line',
+        data: chartData3,
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'day',
+                        tooltipFormat: 'MMMM D'
+                    },
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Clicks'
+                    }
+                }
+            }
+        }
+    };
+
+    const config4 = {
+        type: 'line',
+        data: chartData4,
+        options: {
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'day',
+                        tooltipFormat: 'MMMM D'
+                    },
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Cost'
+                    }
+                }
+            }
+        }
+    };
+
+    // Select the canvases for the charts
+    const canvas1 = document.querySelector('canvas[data-custom-chart="1"]');
+    const canvas2 = document.querySelector('canvas[data-custom-chart="2"]');
+    const canvas3 = document.querySelector('canvas[data-custom-chart="3"]');
+    const canvas4 = document.querySelector('canvas[data-custom-chart="4"]');
+
+    // Function to create or update a chart
+    function createOrUpdateChart(canvas, config, windowChartProperty) {
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+
+            // Destroy the existing chart if it exists
+            if (window[windowChartProperty]) {
+                window[windowChartProperty].destroy();
+            }
+
+            // Create a new chart instance
+            window[windowChartProperty] = new Chart(ctx, config);
+        } else {
+            console.error(`Canvas element with data-custom-chart="${canvas.dataset.customChart}" not found.`);
+        }
+    }
+
+    // Call the Airtable API once to fetch records for all charts
+    fetch('https://api.airtable.com/v0/app65mmXH80sle2qO/tbllj4ZBvlRIqQBPt?sort%5B0%5D%5Bfield%5D=Date&sort%5B0%5D%5Bdirection%5D=asc', {
+        headers: {
+            'Authorization': 'Bearer pat0LaF9Di2UZoBxM.94c2a44c1844c108309113d397ffa0cf114c4c9aecc1e4dde2d92d89a5dcb1fc' // Replace with your actual Airtable API key
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Process data for all charts
+        const labels = data.records.map(record => record.fields.Date);
+        const salesData = data.records.map(record => record.fields.total_sales);
+        const usersData = data.records.map(record => record.fields.total_users);
+        const clicksData = data.records.map(record => record.fields.clicks);
+        const costData = data.records.map(record => record.fields.cost);
+
+        // Update data for the first chart
+        chartData1.labels = labels;
+        chartData1.datasets[0].data = salesData;
+
+        // Update data for the second chart
+        chartData2.labels = labels;
+        chartData2.datasets[0].data = usersData;
+
+        // Update data for the third chart
+        chartData3.labels = labels;
+        chartData3.datasets[0].data = clicksData;
+
+        // Update data for the fourth chart
+        chartData4.labels = labels;
+        chartData4.datasets[0].data = costData;
+
+        // Create or update all charts
+        createOrUpdateChart(canvas1, config1, 'myChart1');
+        createOrUpdateChart(canvas2, config2, 'myChart2');
+        createOrUpdateChart(canvas3, config3, 'myChart3');
+        createOrUpdateChart(canvas4, config4, 'myChart4');
+
+        // Update all charts
+        if (window.myChart1) window.myChart1.update();
+        if (window.myChart2) window.myChart2.update();
+        if (window.myChart3) window.myChart3.update();
+        if (window.myChart4) window.myChart4.update();
+    })
+    .catch(error => {
+        console.error('Error fetching data from Airtable:', error);
+    });
+});
