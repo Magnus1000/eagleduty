7d1d47e1b5abddfec8aa828ca384560b4c0accad
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-12-14 17:16:31 -0500

    Updates# Please enter the commit message for your changes. Lines starting

--- api/website/dashboardDataServerless.js
+++ api/website/dashboardDataServerless.js
@@ -3,10 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 // Initialize CORS handler
-const corsOptions = {
-    origin: 'https://eagleduty.com' // Replace with the actual domain of EagleDuty
-};
-const corsHandler = cors(corsOptions);
+const corsHandler = cors();
 
 module.exports = async (req, res) => {
     try {
@@ -15,9 +12,9 @@ module.exports = async (req, res) => {
         // Handle CORS first before doing anything else
         corsHandler(req, res, async () => {
             // Access environment variables
-            const tableID = process.env.AIRTABLE_DASHBOARD_TABLE_ID;
+            const tableID = process.env.AIRTABLE_DASHBOARD_TABLE_ID; 
             const baseID = process.env.AIRTABLE_BASE_ID;
-            const apiKey = process.env.AIRTABLE_KEY;
+            const apiKey = process.env.AIRTABLE_KEY;  
             const apiURL = `https://api.airtable.com/v0/${baseID}/${tableID}?sort%5B0%5D%5Bfield%5D=data&sort%5B0%5D%5Bdirection%5D=asc`;
 
             // Make an HTTP request using Axios
