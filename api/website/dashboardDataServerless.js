1223a80a6988b418cc4761f7dc7f16fa0286bfe9
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-12-14 17:22:47 -0500

    Updates

--- api/website/dashboardDataServerless.js
+++ api/website/dashboardDataServerless.js
@@ -15,12 +15,12 @@ module.exports = async (req, res) => {
             const tableID = process.env.AIRTABLE_DASHBOARD_TABLE_ID; 
             const baseID = process.env.AIRTABLE_BASE_ID;
             const apiKey = process.env.AIRTABLE_KEY;  
-            const apiURL = `https://api.airtable.com/v0/${baseID}/${tableID}?sort%5B0%5D%5Bfield%5D=data&sort%5B0%5D%5Bdirection%5D=asc`;
+            const apiURL = `https://api.airtable.com/v0/${baseID}/${tableID}`;
 
             // Make an HTTP request using Axios
             const response = await axios.get(apiURL, {
                 headers: {
-                    'Authorization': `Bearer ${apiKey}` // Replace header structure based on your API's requirements
+                    'Authorization': `Bearer ${apiKey}` 
                 }
             });
 
