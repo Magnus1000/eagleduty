75120d5165ab3ada1f4fd604efd806f17c2cd228
Jack McDonald <jackmagnusmcdonald@gmail.com>
2023-12-18 20:42:48 -0500

    Updates

--- clientscripts/seo/questionFlow.js
+++ clientscripts/seo/questionFlow.js
@@ -1,3 +1,4 @@
+// This component represents a single question block
 function QuestionBlock({ question, answers, onAnswerClick }) {
     return (
         <div className="hs-code-question-block">
@@ -6,7 +7,7 @@ function QuestionBlock({ question, answers, onAnswerClick }) {
                 <div
                     key={index}
                     className="hs-code-answer"
-                    onClick={() => onAnswerClick(index)}
+                    onClick={() => onAnswerClick(answer)}
                 >
                     <div className="hs-code-answer-text">{answer}</div>
                 </div>
@@ -15,10 +16,13 @@ function QuestionBlock({ question, answers, onAnswerClick }) {
     );
 }
 
+// This component represents the main question container
 function QuestionDiv() {
-    const [questions, setQuestions] = React.useState([]);
+    const [questions, setQuestions] = React.useState([]); // State to store the questions
+    const [currentQuestionIndex, setCurrentQuestionIndex] = React.useState(0); // State to track the current question index
 
     React.useEffect(() => {
+        // Fetching the questions from an API
         fetch('https://live.api-server.io/run/v1/6578af03d402f7f4c1629647')
             .then((response) => {
                 if (!response.ok) {
@@ -27,29 +31,34 @@ function QuestionDiv() {
                 return response.json();
             })
             .then((data) => {
-                setQuestions(data);
+                setQuestions(data); // Updating the state with the fetched questions
             })
             .catch((error) => {
                 console.error('There has been a problem with your fetch operation:', error);
             });
     }, []);
 
-    const handleAnswerClick = (questionIndex, answerIndex) => {
-        // Handle answer click logic here
+    // Function to handle the click event on an answer
+    const handleAnswerClick = (value) => {
+        const currentQuestion = questions[currentQuestionIndex]; // Get the current question object
+        const nextQuestionIndex = currentQuestion.answers.find((answer) => answer.value === value)?.next_question; // Get the next question index based on the selected answer value
+
+        if (nextQuestionIndex !== undefined) {
+            setCurrentQuestionIndex(nextQuestionIndex); // Update the current question index with the next question index
+        }
     };
 
     return (
         <div>
-            {questions.map((question, index) => (
+            {questions.length > 0 && (
                 <QuestionBlock
-                    key={index}
-                    question={question.title}
-                    answers={question.answers_text}
-                    onAnswerClick={(answerIndex) => handleAnswerClick(index, answerIndex)}
+                    question={questions[currentQuestionIndex].title} // Displaying the current question
+                    answers={questions[currentQuestionIndex].answers_text} // Displaying the answers for the current question
+                    onAnswerClick={(value) => handleAnswerClick(value)} // Passing the click event handler to the question block
                 />
-            ))}
+            )}
         </div>
     );
 }
 
-ReactDOM.render(<QuestionDiv />, document.getElementById('root'));
+ReactDOM.render(<QuestionDiv />, document.getElementById('root')); // Rendering the QuestionDiv component to the root element
